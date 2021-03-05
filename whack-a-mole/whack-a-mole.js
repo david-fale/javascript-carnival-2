6023696204f7d0d52c9c31f8a7d312b103ae411d
{ <david64th@gmail.com>
2021-03-05 14:51:49 +1300

    add whack-a-mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,27 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-
 console.log("Whack-a-Mole!")
+//variables
+cells = document.getElementsByTagName("TD")
+var x =Math.floor(Math.random() * cells.length);
+var randomCell = cells[x];
+
+var moleImage = document.createElement('img');
+moleImage.src = './mole.PNG';
+moleImage.id = "moleImage";
+
+randomCell.appendChild(moleImage);
+
+moleImage.onclick = whackMole;
+
+function whackMole() {
+  x =Math.floor(Math.random() * cells.length);
+  randomCell = cells[x]
+  randomCell.appendChild(moleImage);
+
+  //Play audio on hit
+  var audio = new Audio('./whack-audio.wav');
+  audio.play();
+}
+
