e75a7d9d51a6fa38cec72ce8b3abe74d3b4df56a
{ <david64th@gmail.com>
2021-03-05 13:30:51 +1300

    add inflate-the-unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,33 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-
+var unicorn1 = 0;
+var unicorn2 = 0;
+var unicorn3 = 0;
 console.log("Inflate The Unicorn!")
+
+function inflate() {
+
+    document.getElementById('unicorn1').src = "./images/unicorn-" + unicorn1 + ".png";
+    if (unicorn1 < 3) {
+      unicorn1++;
+    } else {
+      alert("Thank you!");
+    }
+  }
+function inflate2 () {
+  document.getElementById('unicorn2').src = "./images/unicorn-" + unicorn2 + ".png";
+  if (unicorn2 < 3) {
+    unicorn2++;
+  } else {
+    alert("Thank you!");
+  }
+}
+function inflate3 () {
+  document.getElementById('unicorn3').src = "./images/unicorn-" + unicorn3 + ".png";
+  if (unicorn3 < 3) {
+    unicorn3++;
+  } else {
+    alert("Thank you!");
+  }
+}
